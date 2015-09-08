1097abbecab1fea74663c7e23738ba94f8fbc9e4
Agus Darma Kusuma <agusdk2011@gmail.com>
2015-09-08 21:45:59 +0700

    a

---
+++ ikm-engine/ikm-engine-lib/src/main/java/com/myproject/parking/lib/entity/TransactionVO.java
@@ -0,0 +1,338 @@
+package com.myproject.parking.lib.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
+public class TransactionVO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String customerFirstName;
+
+	private String customerLastName;
+
+	private String customerEmail;
+
+	private String customerPhone;
+
+	private String billingFirstName;
+
+	private String billingLastName;
+
+	private String billingAddress;
+
+	private String billingCity;
+
+	private String billingPostalCode;
+
+	private String billingPhone;
+
+	private String billingCountryCode;
+
+	private String shippingFirstName;
+
+	private String shippingLastName;
+
+	private String shippingAddress;
+
+	private String shippingCity;
+
+	private String shippingPostalCode;
+
+	private String shippingPhone;
+
+	private String shippingCountryCode;
+
+	private String paymentOrderId;
+
+	private String paymentTransactionId;
+
+	private Long totalPriceIdr;
+
+	private String paymentMethod;
+
+	private String paymentStatus;
+
+	private String paymentFdsStatus;
+	private String bookingId;
+	private String bookingCode;
+	private int emailNotification;
+	private String emailNotificationReason;
+
+	private Date createdOn;
+	private String createdBy;
+	private Date updatedOn;
+	private String updatedBy;
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCustomerFirstName() {
+		return customerFirstName;
+	}
+
+	public void setCustomerFirstName(String customerFirstName) {
+		this.customerFirstName = customerFirstName;
+	}
+
+	public String getCustomerLastName() {
+		return customerLastName;
+	}
+
+	public void setCustomerLastName(String customerLastName) {
+		this.customerLastName = customerLastName;
+	}
+
+	public String getCustomerEmail() {
+		return customerEmail;
+	}
+
+	public void setCustomerEmail(String customerEmail) {
+		this.customerEmail = customerEmail;
+	}
+
+	public String getCustomerPhone() {
+		return customerPhone;
+	}
+
+	public void setCustomerPhone(String customerPhone) {
+		this.customerPhone = customerPhone;
+	}
+
+	public String getBillingFirstName() {
+		return billingFirstName;
+	}
+
+	public void setBillingFirstName(String billingFirstName) {
+		this.billingFirstName = billingFirstName;
+	}
+
+	public String getBillingLastName() {
+		return billingLastName;
+	}
+
+	public void setBillingLastName(String billingLastName) {
+		this.billingLastName = billingLastName;
+	}
+
+	public String getBillingAddress() {
+		return billingAddress;
+	}
+
+	public void setBillingAddress(String billingAddress) {
+		this.billingAddress = billingAddress;
+	}
+
+	public String getBillingCity() {
+		return billingCity;
+	}
+
+	public void setBillingCity(String billingCity) {
+		this.billingCity = billingCity;
+	}
+
+	public String getBillingPostalCode() {
+		return billingPostalCode;
+	}
+
+	public void setBillingPostalCode(String billingPostalCode) {
+		this.billingPostalCode = billingPostalCode;
+	}
+
+	public String getBillingPhone() {
+		return billingPhone;
+	}
+
+	public void setBillingPhone(String billingPhone) {
+		this.billingPhone = billingPhone;
+	}
+
+	public String getBillingCountryCode() {
+		return billingCountryCode;
+	}
+
+	public void setBillingCountryCode(String billingCountryCode) {
+		this.billingCountryCode = billingCountryCode;
+	}
+
+	public String getShippingFirstName() {
+		return shippingFirstName;
+	}
+
+	public void setShippingFirstName(String shippingFirstName) {
+		this.shippingFirstName = shippingFirstName;
+	}
+
+	public String getShippingLastName() {
+		return shippingLastName;
+	}
+
+	public void setShippingLastName(String shippingLastName) {
+		this.shippingLastName = shippingLastName;
+	}
+
+	public String getShippingAddress() {
+		return shippingAddress;
+	}
+
+	public void setShippingAddress(String shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
+
+	public String getShippingCity() {
+		return shippingCity;
+	}
+
+	public void setShippingCity(String shippingCity) {
+		this.shippingCity = shippingCity;
+	}
+
+	public String getShippingPostalCode() {
+		return shippingPostalCode;
+	}
+
+	public void setShippingPostalCode(String shippingPostalCode) {
+		this.shippingPostalCode = shippingPostalCode;
+	}
+
+	public String getShippingPhone() {
+		return shippingPhone;
+	}
+
+	public void setShippingPhone(String shippingPhone) {
+		this.shippingPhone = shippingPhone;
+	}
+
+	public String getShippingCountryCode() {
+		return shippingCountryCode;
+	}
+
+	public void setShippingCountryCode(String shippingCountryCode) {
+		this.shippingCountryCode = shippingCountryCode;
+	}
+
+	public String getPaymentOrderId() {
+		return paymentOrderId;
+	}
+
+	public void setPaymentOrderId(String paymentOrderId) {
+		this.paymentOrderId = paymentOrderId;
+	}
+
+	public String getPaymentTransactionId() {
+		return paymentTransactionId;
+	}
+
+	public void setPaymentTransactionId(String paymentTransactionId) {
+		this.paymentTransactionId = paymentTransactionId;
+	}
+
+	public Long getTotalPriceIdr() {
+		return totalPriceIdr;
+	}
+
+	public void setTotalPriceIdr(Long totalPriceIdr) {
+		this.totalPriceIdr = totalPriceIdr;
+	}
+
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	public String getPaymentStatus() {
+		return paymentStatus;
+	}
+
+	public void setPaymentStatus(String paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+
+	public String getPaymentFdsStatus() {
+		return paymentFdsStatus;
+	}
+
+	public void setPaymentFdsStatus(String paymentFdsStatus) {
+		this.paymentFdsStatus = paymentFdsStatus;
+	}
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getUpdatedOn() {
+		return updatedOn;
+	}
+
+	public void setUpdatedOn(Date updatedOn) {
+		this.updatedOn = updatedOn;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public String getBookingId() {
+		return bookingId;
+	}
+
+	public void setBookingId(String bookingId) {
+		this.bookingId = bookingId;
+	}
+
+	public String getBookingCode() {
+		return bookingCode;
+	}
+
+	public void setBookingCode(String bookingCode) {
+		this.bookingCode = bookingCode;
+	}
+
+	public int getEmailNotification() {
+		return emailNotification;
+	}
+
+	public void setEmailNotification(int emailNotification) {
+		this.emailNotification = emailNotification;
+	}
+
+	public String getEmailNotificationReason() {
+		return emailNotificationReason;
+	}
+
+	public void setEmailNotificationReason(String emailNotificationReason) {
+		this.emailNotificationReason = emailNotificationReason;
+	}
+
+}
---
+++ ikm-engine/ikm-engine-lib/src/main/java/com/myproject/parking/lib/mapper/TransactionMapper.java
@@ -0,0 +1,15 @@
+package com.myproject.parking.lib.mapper;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.myproject.parking.lib.entity.TransactionVO;
+
+public interface TransactionMapper {
+	public void createTransaction(TransactionVO transactionVO);
+	public void updateTransactionStatus(@Param("PaymentTransactionId") String PaymentTransactionId
+			,@Param("PaymentFdsStatus") String PaymentFdsStatus
+			,@Param("PaymentStatus") String PaymentStatus,@Param("orderId") String orderId);
+	public void updateEmailNotification(@Param("emailNotification") int emailNotification,@Param("emailNotificationReason") String emailNotificationReason,
+			@Param("orderId") String orderId);
+    
+}
