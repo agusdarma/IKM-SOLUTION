b47341dfe8fa9bbbd3695f43ce29b4ebcb8c29f4
Agus Darma Kusuma <agusdk2011@gmail.com>
2015-09-13 22:59:20 +0700

    API Speck

---
+++ ikm-engine/api/loginUser/response/RespLoginVO.java
@@ -0,0 +1,67 @@
+package com.myproject.ikm.lib.data;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
+public class RespLoginVO implements java.io.Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+	private String kodeSekolah;
+	private String noInduk;
+	private String nama;
+	private int userType;
+	private int statusUser;
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getKodeSekolah() {
+		return kodeSekolah;
+	}
+
+	public void setKodeSekolah(String kodeSekolah) {
+		this.kodeSekolah = kodeSekolah;
+	}
+
+	public String getNoInduk() {
+		return noInduk;
+	}
+
+	public void setNoInduk(String noInduk) {
+		this.noInduk = noInduk;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public int getUserType() {
+		return userType;
+	}
+
+	public void setUserType(int userType) {
+		this.userType = userType;
+	}
+
+	public int getStatusUser() {
+		return statusUser;
+	}
+
+	public void setStatusUser(int statusUser) {
+		this.statusUser = statusUser;
+	}
+}
