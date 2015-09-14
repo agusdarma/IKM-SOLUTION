4f65811c185cf7352719f4cbed794600761247c9
Agus Darma Kusuma <agusdk2011@gmail.com>
2015-09-15 01:07:42 +0700

    a

---
+++ ikm-android/AndroidLoginAndRegistration/src/com/ikm/data/InboxVO.java
@@ -0,0 +1,64 @@
+package com.ikm.data;
+
+import java.util.Date;
+
+public class InboxVO implements java.io.Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String fromName;
+	private String toName;
+	private String isiMessage;
+	private int isRead;
+	private Date createdOn;
+	private boolean self;
+
+	public String getFromName() {
+		return fromName;
+	}
+
+	public void setFromName(String fromName) {
+		this.fromName = fromName;
+	}
+
+	public String getToName() {
+		return toName;
+	}
+
+	public void setToName(String toName) {
+		this.toName = toName;
+	}
+
+	public String getIsiMessage() {
+		return isiMessage;
+	}
+
+	public void setIsiMessage(String isiMessage) {
+		this.isiMessage = isiMessage;
+	}
+
+	public int getIsRead() {
+		return isRead;
+	}
+
+	public void setIsRead(int isRead) {
+		this.isRead = isRead;
+	}
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public boolean isSelf() {
+		return self;
+	}
+
+	public void setSelf(boolean self) {
+		this.self = self;
+	}
+
+	
+}
