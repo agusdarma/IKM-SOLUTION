9ecdb6fedcebfab26f9456683e0b8123fbfc023c
Agus Darma Kusuma <agusdk2011@gmail.com>
2015-09-30 10:13:32 +0700

    a

---
+++ ikm-android-v2/MyAgenda/src/com/ikm/myagenda/data/Constant.java
@@ -0,0 +1,8 @@
+package com.ikm.myagenda.data;
+
+public class Constant {
+	public static final int DRAWER_ITEM_ID_HOME = 10;
+	public static final int DRAWER_ITEM_ID_CHANGE_PASSWORD = 11;
+	public static final int DRAWER_ITEM_ID_BROWSE = 12;
+	public static final int DRAWER_ITEM_ID_UPLOAD = 13;
+}
